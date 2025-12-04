5675504225904fa049f97f732e01227432335b08
mounawaraalicolo5-or <mounawaraalicolo5@gmail.com>
2025-12-04 16:06:30 +0300

    j'ai fait un exercice avec la page gestion de bibliotheque

---
+++ gestionBibliotheque.js
@@ -0,0 +1,19 @@
+let totalLivres=500;
+console.log(totalLivres)
+
+// acheter 500 livres
+totalLivres=totalLivres+50;
+ // en achete 10
+ totalLivres=totalLivres-10;
+ // racheter 5 livres jetés
+ totalLivres=totalLivres+5;
+ //vérification du résultat
+ console.log(totalLivres);
+
+
+
+ let affichageTotalLivres="le nombre total est :"+totalLivres;
+ //affichage de phrase
+ console.log(affichageTotalLivres);
+
+ 
---
+++ script.js
@@ -0,0 +1,71 @@
+//Ici, je rédige mon script JavaScript
+
+ let nom ="Mchagama";
+        let prenom ="mchangama";
+
+        /*declare mon age*/
+        let age= 18;
+
+        console.log("Bonjour, je m'appelle ",prenom,nom,age,);
+        let prix = 12 ;
+        console.log("prix",prix);
+        const nombredecimale = 3.4123;
+        const nombreNegative = -509;
+        const nombreFraction = 1/3;
+        console.log("Nombre decimale :",nombredecimale);
+        console.log("Nombre negative :",nombreNegative);
+        console.log("Nombre Nraction :",nombreFraction);
+        
+
+        let salutation ="Bonjour les gens";
+        console.log("salutation",salutation);
+
+        let commune ="Boueni";
+        console.log("commune",commune);
+
+        let village ="Hagnoundrou";
+        console.log("village",village);
+
+        let greeting ="good morning";
+        console.log("greeting",greeting);
+
+        let andabu ="kwezi";
+        console.log("andabu",andabu);
+
+        let vrai =true;
+        console.log(vrai);
+
+        let est_faux =false;
+        console.log(est_faux);
+        /* liste du tableau encadrée par personne*/
+
+        let eleves = ["Mouna","Fatima","Marwan","Irwan","Dhinouraine"];
+        /*affiche mon tableau:*/
+        console.log(eleves);
+
+        //indice ici le nombre
+        console.log(eleves[0]);
+
+        console.log(eleves[1]);
+
+        console.log(eleves[2]);
+
+        //j'ai recuperer le 3 eme membre du tableau
+        console.log(eleves[3]);
+
+        console.log(eleves[4]);
+
+        let client ={
+            nom:"Mouna",
+            village:"Hagnoundrou",
+            age:19,
+            scores:[5,9,2]
+        };
+
+        console.log(client);
+        /*element de notre client*/
+        console.log(client.nom);
+        console.log(client.village);
+        console.log(client.age);
+        console.log(client.scores);
+        console.log(client.scores[9]);
